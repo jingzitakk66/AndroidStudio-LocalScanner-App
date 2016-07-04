e8153bd0ff784d1fa481e94cc9da830211a7def4
HiveMind <Zerodavila11@gmail.com>
2016-07-04 04:54:32 -0400

    Add files via upload

--- app/src/main/java/com/hivemind/zerodavila/localscanner/activity/response/MainAsyncResponse.java
+++ app/src/main/java/com/hivemind/zerodavila/localscanner/activity/response/MainAsyncResponse.java
@@ -4,24 +4,9 @@ import java.util.Map;
 
 public interface MainAsyncResponse {
 
-    /**
-     * Delegate to handle map outputs
-     *
-     * @param output
-     */
+    //Delegate to handle map outputs
     void processFinish(Map<String, String> output);
 
-    /**
-     * Delegate to handle integer outputs
-     *
-     * @param output
-     */
+    //Delegate to handle integer outputs
     void processFinish(int output);
-
-    /**
-     * Delegate to handle string outputs
-     *
-     * @param output
-     */
-    void processFinish(String output);
 }
